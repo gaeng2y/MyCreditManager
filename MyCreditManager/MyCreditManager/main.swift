bb6ff84773d1e04ba570903ab15c9c6297e388d3
gaeng2y <gaeng2y@gmail.com>
2022-11-21 19:55:06 +0900

    2. 학생 삭제

--- MyCreditManager/MyCreditManager/main.swift
+++ MyCreditManager/MyCreditManager/main.swift
@@ -16,10 +16,12 @@ func start() {
         print("원하는 기능을 입력해주세요\n1: 학생추가, 2: 학생삭제, 3: 성적추가(변경), 4: 성적삭제, 5: 평점보기, X: 종료")
         let menu = getInput()
         switch menu {
-        case .exit:
-            break
         case .addStudent:
             addStudent()
+        case .deleteStudent:
+            deleteStudent()
+        case .exit:
+            break
         default:
             continue
         }
@@ -71,6 +73,23 @@ private func addStudent() {
         students.append(student)
         
         print("\(input) 학생을 추가했습니다.")
+    }
+}
+
+private func deleteStudent() {
+    guard let input = readLine() else {
+        printAbnormalError()
+        return
+    }
+    
+    let student = Student(name: input)
+    
+    if isExistStudent(student) {
+        students.removeAll { $0.name == student.name }
+        
+        print("\(input) 학생을 삭제했습니다.")
+    } else {
+        print("\(input) 학생을 찾기 못했습니다.")
     }
 }
 
